71609b9e174d935d0359b0ac4e82a0512bb4faac
Israel Llorens <sanchezis@hotmail.com>
2025-03-24 03:29:27 +0100

    done

--- digital_pathology/process/stardist.py
+++ digital_pathology/process/stardist.py
@@ -20,23 +20,50 @@ from pyspark.sql.types import BooleanType
 class NucleotidExtractor(object):
     def __init__(self):
         from stardist.models import StarDist2D
-        self.model_he = StarDist2D.from_pretrained('2D_versatile_he')
         return
 
     def run(self, spark):
         @udf(BooleanType())
-        def verify_StarDist2D():
+        def extract_nucleotid(col):
+            import skimage
             from stardist.models import StarDist2D
+            from stardist.data import test_image_nuclei_2d
+            from stardist.plot import render_label
+            from csbdeep.utils import normalize
+            
+            # Load model once per executor process
+            if not hasattr(extract_nucleotid, 'model_he'):
+                extract_nucleotid.model_he = StarDist2D.from_pretrained('2D_versatile_he')
+            model_he = extract_nucleotid.model_he
+            
+            image_file_name = col
+            image = skimage.io.imread(image_file_name)
+            
+            try:
+                labels, _ = model_he.predict_instances(normalize(image))
+                skimage.io.imsave(f'{image_file_name}_nucleotid_labels.png', labels)
+            except:
+                return False
             
             return True
 
         # Check installation across cluster
-        spark.range(10).withColumn("StarDist2D_installed", verify_StarDist2D()).show()    
-
-
-
-
-
         downloaded = spark.read.parquet(os.path.join('data', '1-download.parquet'))
 
         downloaded.show(truncate=False)
+
+        downloaded = downloaded\
+                        .repartition(2)\
+                        .withColumn(
+                            "nucleotid_filename", 
+                            extract_nucleotid(F.concat( F.lit(
+                                os.path.join(
+                                    'data',
+                                    'patient_extracts', 
+                                    )),
+                                F.lit(os.path.sep),
+                                F.col('filename') ))
+                        )
+
+        downloaded.write.mode('overwrite').parquet(os.path.join('data', '2-stardist-nucleotids.parquet'))
+
